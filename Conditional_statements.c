02b007241e2c809ca364cd285c3776d9923edfa4
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-11-20 22:43:43 +0530

    Add conditional statements examples in C
    Added syntax examples for if, else if, and else statements along with a sample program demonstrating comparisons between two integers.

---
+++ Conditional_statements.c
@@ -0,0 +1,34 @@
+// syntex of if condition 
+if (condition)
+    {
+        // code
+        // do something
+    }
+  
+// syntex of else if 
+else if ()
+     {
+       // do something
+       // do something
+     }
+
+// syntex of else 
+else 
+  // do something 
+
+ // small code
+#include<stdio.h>
+  int main (){
+int a = 4 ;
+int b = 5 ;
+if (a > b){
+    printf("a is greater then b ");
+      }
+else if (a==b){
+     printf (" a is equal to b");
+}
+else 
+  printf("a is smaller then b");
+return 0;
+}
+        
