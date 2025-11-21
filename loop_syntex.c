7d9f4e1e20da3fb68e8e455746314ea3440c5721
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-11-21 21:49:46 +0530

    Add syntax examples for loop structures in C
    syntex of loops

---
+++ loop_syntex.c
@@ -0,0 +1,15 @@
+//syntex of 'for' loop :
+for(start; condition; increment) 
+        {
+          //do something
+        }
+
+//syntex of 'while' loop :
+while(condition) {
+    //do something
+     // increment
+   }
+
+//syntex of 'do while' loop :
+do {'operation' }
+  while(condition);
