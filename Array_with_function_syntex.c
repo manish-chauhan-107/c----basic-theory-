610b28a1206c9c3b8fb5e340171046e61bb71e6f
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-12-10 18:41:06 +0530

    Correct display function and return statement
    Fix function definition and call in Array_with_function_syntex.c

--- Array_with_function_syntex.c
+++ Array_with_function_syntex.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+//function decleration;
+void display(int arr[], int size);
 
+//function defination :
+void display(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+int main (){
+int a[10] = {1,2,3,4,5,6,7,8,9,0};
+//function call :
+display(a, 5);   // passing array to function
+reyurn 0 ;
+}
